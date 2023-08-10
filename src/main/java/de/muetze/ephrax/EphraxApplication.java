8a3d37d89ad154c82a5c86bf3a20790d858230d1
Wayssm <aaron.muetze@outlook.com>
2023-08-10 22:32:29 +0200

    init

---
+++ src/main/java/de/muetze/ephrax/EphraxApplication.java
@@ -0,0 +1,13 @@
+package de.muetze.ephrax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EphraxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EphraxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/muetze/ephrax/EphraxApplicationTests.java
@@ -0,0 +1,13 @@
+package de.muetze.ephrax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EphraxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
