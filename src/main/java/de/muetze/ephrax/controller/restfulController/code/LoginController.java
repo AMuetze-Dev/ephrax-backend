36c64aea4754a7d93e26d4e50031b23fa862cba3
Wayssm <aaron.muetze@outlook.com>
2023-08-14 22:00:34 +0200

    autopush

---
+++ src/main/java/de/muetze/ephrax/controller/restfulController/code/LoginController.java
@@ -0,0 +1,56 @@
+package de.muetze.ephrax.controller.restfulController.code;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+	@PostMapping("/login")
+	public LoginResponse login(@RequestBody LoginQuestion question) {
+
+		return new LoginResponse("test123");
+	}
+}
+
+class LoginQuestion {
+
+	private String username;
+	private String password;
+
+	LoginQuestion(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	String getPassword() {
+		return password;
+	}
+
+	String getUsername() {
+		return username;
+	}
+
+	void setPassword(String password) {
+		this.password = password;
+	}
+
+	void setUsername(String username) {
+		this.username = username;
+	}
+
+}
+
+class LoginResponse {
+
+	private final String answer;
+
+	LoginResponse(String answer) {
+		this.answer = answer;
+	}
+
+	String getAnswer() {
+		return answer;
+	}
+}
